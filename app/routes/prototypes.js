84718d3ea63a3cc33f67ab9c953afb22f9db32df
Mike Ballan <info@mikeballan.co.uk>
2016-05-11 18:10:33 +0100

    Adds routes js for forms, prototypes and stylekit

---
+++ app/routes/prototypes.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import ScrollToTop from '../mixins/scroll-to-top';
+
+export default Ember.Route.extend(ScrollToTop, {
+  setupController(controller) {
+    controller.set('headerTitle', 'Prototype');
+    controller.set('headerCopy', 'Here you can see some of the things weve been playing around with that might one day be a part of Amaziums core.');
+  },
+});
