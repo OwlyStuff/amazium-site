c797891bd11dd9d5abbb62502d13db1d2e9ce131
Mike Ballan <info@mikeballan.co.uk>
2016-05-11 18:19:59 +0100

    Adds header info for the structure page

--- app/routes/structure.js
+++ app/routes/structure.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+import ScrollToTop from '../mixins/scroll-to-top';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ScrollToTop, {
+  setupController(controller) {
+    controller.set('headerTitle', 'The Amazium Grid');
+    controller.set('headerCopy', 'With Amaziums default 12-column responsive layout you can create almost any layout you can think of.');
+  },
 });
