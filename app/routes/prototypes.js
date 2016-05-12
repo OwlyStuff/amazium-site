b73cfe5cf7f8287d4a75f96be191a63ed50f333a
Mike Ballan <info@mikeballan.co.uk>
2016-05-12 14:40:37 +0100

    Fixes typos

--- app/routes/prototypes.js
+++ app/routes/prototypes.js
@@ -4,6 +4,6 @@ import ScrollToTop from '../mixins/scroll-to-top';
 export default Ember.Route.extend(ScrollToTop, {
   setupController(controller) {
     controller.set('headerTitle', 'Prototype');
-    controller.set('headerCopy', 'Here you can see some of the things weve been playing around with that might one day be a part of Amaziums core.');
+    controller.set('headerCopy', 'Here you can see some of the things weve been playing around with that might one day be a part of Amazium\'s core.');
   },
 });
