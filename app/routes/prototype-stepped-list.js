1fb903a3dee84aaff06de1b242de9662ba01e282
Graeme Chapman <graeme.chapman@infinitycloud.com>
2016-05-20 13:22:25 +0000

    [Templates] Fixed spelling and grammar.

--- app/routes/prototype-stepped-list.js
+++ app/routes/prototype-stepped-list.js
@@ -4,6 +4,6 @@ import ScrollToTop from '../mixins/scroll-to-top';
 export default Ember.Route.extend(ScrollToTop, {
   setupController(controller) {
     controller.set('headerTitle', 'Stepped list');
-    controller.set('headerCopy', 'Nice and simple ordered list styled to look like a forms progress. All you need to do is add the class .active to the relevant li and the rest is handled by the CSS.');
+    controller.set('headerCopy', 'Nice and simple ordered list styled to look like a forms progress. All you need to do is add the class <code>.active</code> to the relevant li and the rest is handled by the CSS.');
   },
 });
