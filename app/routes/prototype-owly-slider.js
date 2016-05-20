75451b17bfed1d446eac7f18c22de2f9f164f69b
Graeme Chapman <graeme.chapman@infinitycloud.com>
2016-05-20 14:46:31 +0000

    Update slideshow header text to parse as HTML

--- app/routes/prototype-owly-slider.js
+++ app/routes/prototype-owly-slider.js
@@ -4,6 +4,8 @@ import ScrollToTop from '../mixins/scroll-to-top';
 export default Ember.Route.extend(ScrollToTop, {
   setupController(controller) {
     controller.set('headerTitle', 'Owly Slider');
-    controller.set('headerCopy', 'This awesome slideshow was made by <a href="https://github.com/shahrukhomar">Shahrukh Omar</a> and is so simple it hurts. Just remeber to include the owly-slider.js file to your webpage and the stylesheet owly-slider.css.');
+    controller.set('headerCopy', Ember.computed(function() {
+      return Ember.String.htmlSafe('This awesome slideshow was made by <a href="https://github.com/shahrukhomar">Shahrukh Omar</a> and is so simple it hurts. Just remember to include the owly-slider.js file to your webpage and the stylesheet owly-slider.css.');
+    }));
   },
 });
