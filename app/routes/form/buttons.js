38ead5cccba328e4c5d24a2094ce09df7ddfd299
Mike Ballan <info@mikeballan.co.uk>
2016-06-22 11:18:46 +0100

    Updaes the header and intro block on the homepage

--- app/routes/form/buttons.js
+++ app/routes/form/buttons.js
@@ -3,7 +3,7 @@ import ScrollToTop from '../../mixins/scroll-to-top';
 
 export default Ember.Route.extend(ScrollToTop, {
   setupController(controller) {
-    controller.set('headerTitle', 'Styled Links, Inputs & Buttons');
+    controller.set('headerTitle', 'Links, Inputs & Buttons');
     controller.set('headerCopy', 'These are the most traditional element for actions on a website and just like everything else Amazium have created simple styles for all your needs.');
   },
 });
