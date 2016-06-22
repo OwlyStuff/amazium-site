993efdd01a9067c38a5381353346d90ecc22eab2
Mike Ballan <info@mikeballan.co.uk>
2016-06-22 12:04:49 +0100

    Fixes typo and updates homepage images

--- app/routes/stylekit/colors.js
+++ app/routes/stylekit/colors.js
@@ -4,6 +4,6 @@ import ScrollToTop from '../../mixins/scroll-to-top';
 export default Ember.Route.extend(ScrollToTop, {
   setupController(controller) {
     controller.set('headerTitle', 'Colors');
-    controller.set('headerCopy', 'Amazium comes with a collection of colors form Positive to Negative colors.');
+    controller.set('headerCopy', 'Amazium comes with a collection of colors from Positive to Negative colors.');
   },
 });
